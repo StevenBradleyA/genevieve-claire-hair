cde8101bfaeae14f5ddaf03a9d75928e44a8554e
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-07-22 16:19:32 -0700

    👕 created new image rendering style with an inplace zoom effect 👕

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -8,11 +8,19 @@ interface InstagramFeedItem {
     caption: string | null;
 }
 
+interface InstagramCarouselItem {
+    id: string;
+    media_url: string;
+    media_type: "IMAGE" | "CAROUSEL_ALBUM";
+}
+
 interface InstagramApiResponse {
     data: {
         id: string;
+        media_type: "IMAGE" | "CAROUSEL_ALBUM";
         media_url: string;
         caption: string | null;
+        children?: { data: InstagramCarouselItem[] };
     }[];
 }
 
@@ -32,8 +40,33 @@ export default function Portfolio() {
 
     return (
         <>
-            <h1 className="text-white text-6xl">Insta </h1>
-            <div className="grid grid-cols-3 gap-4">
+            <h1 className="text-6xl text-white">Insta </h1>
+
+            <div className="flex flex-wrap">
+                {instaFeed.map((post) => (
+                    <div
+                        key={post.id}
+                        className="w-full overflow-hidden sm:w-1/2 md:w-1/3 lg:w-1/4"
+                        style={{ width: "300px", height: "300px" }}
+                    >
+                        <div className="zoom-effect h-full w-full rounded-lg object-cover">
+                            <Image
+                                src={post.media_url}
+                                alt={post.caption || ""}
+                                width={300}
+                                height={300}
+                                className="h-full w-full"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
+// caption hover idea
+{
+    /* <div className="grid grid-cols-3 gap-4">
                 {instaFeed.map((post) => (
                     <div key={post.id} className="group relative">
                         <Image
@@ -42,18 +75,16 @@ export default function Portfolio() {
                             width={300}
                             height={300}
                         />
-                        {/* <img src={post.media_url} alt={post.caption || ""} /> */}
+                        <img src={post.media_url} alt={post.caption || ""} />
 
-                        {/* <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                             <div className="rounded-md bg-gray-800 p-2 text-center text-white">
                                 {post.caption || "No caption"}
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 ))}
-            </div>
-        </>
-    );
+            </div> */
 }
 
 async function fetchInstagramFeed(
@@ -61,14 +92,38 @@ async function fetchInstagramFeed(
 ): Promise<InstagramFeedItem[]> {
     try {
         const response = await fetch(
-            `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url&access_token=${accessToken}`
+            `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,children{media_type,media_url}&access_token=${accessToken}`
         );
         const data = (await response.json()) as InstagramApiResponse;
-        return data.data.map((post) => ({
-            id: post.id,
-            media_url: post.media_url,
-            caption: post.caption ? post.caption : "No caption",
-        }));
+
+        const filteredData = data.data.filter(
+            (post) =>
+                post.media_type === "IMAGE" ||
+                post.media_type === "CAROUSEL_ALBUM"
+        );
+
+        const feedItems: InstagramFeedItem[] = [];
+        for (const post of filteredData) {
+            if (post.media_type === "IMAGE") {
+                feedItems.push({
+                    id: post.id,
+                    media_url: post.media_url,
+                    caption: post.caption ? post.caption : "No caption",
+                });
+            } else if (post.media_type === "CAROUSEL_ALBUM" && post.children) {
+                for (const child of post.children.data) {
+                    if (child.media_type === "IMAGE") {
+                        feedItems.push({
+                            id: child.id,
+                            media_url: child.media_url,
+                            caption: post.caption ? post.caption : "No caption",
+                        });
+                    }
+                }
+            }
+        }
+
+        return feedItems;
     } catch (error) {
         throw new Error("Error fetching Instagram feed");
     }
