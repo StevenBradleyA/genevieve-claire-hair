b73b90eb7d0f677bc0af1528ad6fb6a8367817af
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-07-22 14:43:50 -0700

    Refactored Portfolio so that it is typesafe 👐

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { env } from "~/env.mjs";
+import Image from "next/image";
+
+interface InstagramFeedItem {
+    id: string;
+    media_url: string;
+    caption: string;
+}
+
+interface InstagramApiResponse {
+    data: {
+        id: string;
+        media_url: string;
+        caption: string | null;
+    }[];
+}
 
 export default function Portfolio() {
     const instaToken = env.NEXT_PUBLIC_INSTA_TOKEN;
-    const instaSecret = env.NEXT_PUBLIC_INSTA_SECRET;
-    const instaAppId = env.NEXT_PUBLIC_INSTA_APP_ID;
-    const [instaFeed, setInstaFeed] = useState([]);
+    const [instaFeed, setInstaFeed] = useState<InstagramFeedItem[]>([]);
 
     useEffect(() => {
         if (instaToken) {
@@ -19,11 +32,11 @@ export default function Portfolio() {
 
     return (
         <>
-           <h1>Instagram Feed</h1>
+            <h1>Instagram Feed</h1>
             <div>
                 {instaFeed.map((post) => (
                     <div key={post.id}>
-                        <img src={post.media_url} alt={post.caption} />
+                        <Image src={post.media_url} alt={post.caption} />
                         <p>{post.caption}</p>
                     </div>
                 ))}
@@ -32,12 +45,14 @@ export default function Portfolio() {
     );
 }
 
-async function fetchInstagramFeed(accessToken) {
+async function fetchInstagramFeed(
+    accessToken: string
+): Promise<InstagramFeedItem[]> {
     try {
         const response = await fetch(
             `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url&access_token=${accessToken}`
         );
-        const data = await response.json();
+        const data = (await response.json()) as InstagramApiResponse;
         return data.data.map((post) => ({
             id: post.id,
             media_url: post.media_url,
