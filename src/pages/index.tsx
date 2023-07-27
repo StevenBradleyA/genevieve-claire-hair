af985f910da84c3cc78c6813375fa66b009ec1d3
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-07-27 12:24:06 -0700

    added new custom 3D scene to homepage and created a zoom function 🆕

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import lsp3 from "../../public/3.png";
 import greenLogo from "../../public/gch-green-logo.png";
 import holo from "../../public/Holographic/holo-wavy.png";
 import { useState } from "react";
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 export default function Home() {
     // TODO should we use Webfont Loader??? Might load google fonts asynchronously and be more optimized
@@ -37,8 +37,6 @@ export default function Home() {
 
     // ----------------------------------------------------------------------------------------------------------------
 
-
-
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const images = [lsp1, lsp2, lsp3];
 
@@ -53,19 +51,12 @@ export default function Home() {
             <h1 className="mb-5 font-grand-hotel text-9xl text-white">
                 Genevieve Clare Hair
             </h1>
-            <div className=" w-full">
-    {/* <Spline scene="https://prod.spline.design/bunTWGtXBDALSsjq/scene.splinecode" /> */}
-  <Spline scene="https://prod.spline.design/0xgZT5ytuvKdzqdh/scene.splinecode" />
+            <div className=" w-1/2">
+                <Spline scene="https://prod.spline.design/0xgZT5ytuvKdzqdh/scene.splinecode" />
             </div>
 
             <div className="flex items-center">
-                <div className=" ml-auto flex w-1/2 justify-center pl-10">
-                  
-                    {/* <h1 className="pog font-quattrocento text-8xl text-white animate-glitch">
-                    <h1 className="pog animate-glitch font-quattrocento text-8xl text-white">
-                        No bad Hair days
-                    </h1> */}
-                </div>
+                <div className=" ml-auto flex w-1/2 justify-center pl-10"></div>
                 <div className="ml-auto flex h-96 w-1/2 justify-center pr-10">
                     <Image
                         src={holo}
