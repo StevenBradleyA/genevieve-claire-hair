099654108cf026b487ae4011b8d09e36b2b91d34
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-02 10:37:54 -0700

    created notes and plan for admin page layout with features ✍️

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,6 +3,27 @@ export default function AdminPage() {
 // TODO add admin only viewing or redirect if user is not admin 
 
 
+// Want ability to change pricing page
+// Geni can add stuff to calendar in the booking page probs
+
+// maybe a calendar with a view? 
+
+// custom checkout here? custom calculator 
+// product drop down with checkout too. 
+// select service + additional time. 
+
+// would want to be able to close off books here or change calendar availability 
+
+// square checkout implementation 
+
+// list all clients alphabetically with a search 
+// clicking on a client opens a admin/clientId/# that shows clients prev services with date with photos of client and client notes
+
+
+// purchase log 
+// sell product through this page???? 
+
+
     return (
         <>
             <div> hello </div>
