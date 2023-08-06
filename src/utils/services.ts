c7d4d6907ea896eb4d71f9dd4315ef3db41d35db
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-06 10:23:30 -0700

    Service update w/ Geni

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -11,6 +11,12 @@
  * All choices shown
  */
 
+/**
+ * Blonding includes highlights, balayage, baby lights, bleach and tone (User input for more info)
+ * Full - $220 => 360, Partial - $170 => 310
+ * 
+ * Any bundles are - $10
+ */
 
 const allServices: { [key: string]: any } = {
     haircut: {
@@ -41,6 +47,7 @@ const allServices: { [key: string]: any } = {
             select: false,
             time: 90,
             price: 90,
+            bundleTime: 60,
         },
     },
     allOverColor: {
@@ -65,58 +72,21 @@ const allServices: { [key: string]: any } = {
             name: "Roots to ends",
             select: false,
             time: 120,
-            price: 105,
-        },
-    },
-    highlights: {
-        name: "Highlights",
-        select: false,
-        rate: 95,
-        disable: ["balayage", "bleachTone", "babyLights"],
-        partial: {
-            name: "Partial",
-            select: false,
-            time: 150,
-            price: 140,
-        },
-
-        full: {
-            name: "Full",
-            select: false,
-            time: 180,
-            price: 220,
-        },
-    },
-    balayage: {
-        name: "Balayage",
-        select: false,
-        rate: 95,
-        disable: ["bleachTone", "babyLights", "highlights"],
-        partial: {
-            name: "Partial",
-            select: false,
-            time: 180,
-            price: 215,
-        },
-        full: {
-            name: "Full",
-            select: false,
-            time: 180,
-            price: 260,
+            price: 115,
         },
     },
     vivids: {
         name: "Vivids",
         select: false,
-        rate: 110,
-        price: 110,
+        rate: 110, // per hour
+        price: 135,
         requireConsult: true,
     },
     colorCorrections: {
         name: "Color Corrections",
         select: false,
-        rate: 130,
-        price: 130,
+        rate: 150, // per hour
+        price: 150,
         requireConsult: true,
     },
     styling: {
@@ -128,7 +98,7 @@ const allServices: { [key: string]: any } = {
             name: "Blowout",
             select: false,
             time: 40,
-            price: 45,
+            price: 45, // starting at
         },
         specialEvent: {
             name: "Special Event - Prom, Homecoming, Senior pics, Formal",
@@ -145,6 +115,7 @@ const allServices: { [key: string]: any } = {
         },
     },
     extensions: {
+        // Geni manually adds only
         name: "Extensions",
         select: false,
         rate: 70,
@@ -167,51 +138,90 @@ const allServices: { [key: string]: any } = {
             price: 70,
         },
     },
-    babyLights: {
-        name: "Baby Lights",
-        select: false,
-        rate: 100,
-        disable: ["balayage", "bleachTone", "highlights"],
-        partial: {
-            name: "Partial",
-            select: false,
-            time: 210,
-            price: 250,
-        },
-        full: {
-            name: "Full",
-            select: false,
-            time: 270,
-            price: 290,
-        },
-    },
-    bleachTone: {
-        name: "Bleach & Tone",
-        select: false,
-        rate: 85,
-        disable: ["balayage", "allOverColor", "babyLights", "highlights"],
-        halfInch: {
-            name: "Growouts: 1/2 inch or less",
-            select: false,
-            time: 150,
-            price: 140,
-        },
-        twoInch: {
-            name: "Growouts: 1/2 inch to 2 inches",
-            select: false,
-            time: 180,
-            price: 190,
-            message: true,
-        },
-        moreThanTwo: {
-            name: "Growouts: 2 inches or more",
-            select: false,
-            requireConsult: true,
-        },
-    },
+
     quiet: {
         name: "Quiet Appointment",
         select: false,
         rate: 0,
     },
 };
+
+// highlights: {
+//     name: "Highlights",
+//     select: false,
+//     rate: 95,
+//     disable: ["balayage", "bleachTone", "babyLights"],
+//     partial: {
+//         name: "Partial",
+//         select: false,
+//         time: 150,
+//         price: 140,
+//     },
+
+//     full: {
+//         name: "Full",
+//         select: false,
+//         time: 180,
+//         price: 220,
+//     },
+// },
+// balayage: {
+//     name: "Balayage",
+//     select: false,
+//     rate: 95,
+//     disable: ["bleachTone", "babyLights", "highlights"],
+//     partial: {
+//         name: "Partial",
+//         select: false,
+//         time: 180,
+//         price: 215,
+//     },
+//     full: {
+//         name: "Full",
+//         select: false,
+//         time: 180,
+//         price: 260,
+//     },
+// },
+// babyLights: {
+//     name: "Baby Lights",
+//     select: false,
+//     rate: 100,
+//     disable: ["balayage", "bleachTone", "highlights"],
+//     partial: {
+//         name: "Partial",
+//         select: false,
+//         time: 210,
+//         price: 250,
+//     },
+//     full: {
+//         name: "Full",
+//         select: false,
+//         time: 270,
+//         price: 290,
+//     },
+// },
+// bleachTone: {
+//     name: "Bleach & Tone",
+//     select: false,
+//     rate: 85,
+//     disable: ["balayage", "allOverColor", "babyLights", "highlights"],
+//     halfInch: {
+//         name: "Growouts: 1/2 inch or less",
+//         select: false,
+//         time: 150,
+//         price: 140,
+//     },
+//     twoInch: {
+//         name: "Growouts: 1/2 inch to 2 inches",
+//         select: false,
+//         time: 180,
+//         price: 190,
+//         message: true,
+//     },
+//     moreThanTwo: {
+//         name: "Growouts: 2 inches or more",
+//         select: false,
+//         requireConsult: true,
+//     },
+// },
