e57233b4cfa81ba432130466263fdb419cd5d796
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-06 12:54:45 -0700

    create new checkbox animation and added styling 🎁

--- src/components/NewClientForm/ServiceOptions.tsx
+++ src/components/NewClientForm/ServiceOptions.tsx
@@ -25,54 +25,56 @@ export default function ServiceOptions() {
             <div className="font-quattrocento text-3xl text-white ">
                 Which services do you see yourself using?
                 <label>(Select all that you might want)</label>
-                <div>
-                    <label className=" cursor-pointer">
+                <div className="flex gap-10">
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Haircuts
-                            <input
-                                type="checkbox"
-                                name="haircuts"
-                                checked={formData.haircuts}
-                                onChange={(e) => toggle(e.target.name)}
-                                className="custom-checkbox"
-                            />
+                        <input
+                            type="checkbox"
+                            name="haircuts"
+                            checked={formData.haircuts}
+                            onChange={(e) => toggle(e.target.name)}
+                            className="custom-checkbox"
+                        />
                     </label>
-                    <label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Color
                         <input
                             type="checkbox"
                             name="color"
                             checked={formData.color}
                             onChange={(e) => toggle(e.target.name)}
-                        ></input>
+                            className="custom-checkbox"
+                        />
                     </label>
-                    <label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Vivid
                         <input
                             type="checkbox"
                             name="vivid"
                             checked={formData.vivid}
                             onChange={(e) => toggle(e.target.name)}
-                        ></input>
+                            className="custom-checkbox"
+                        />
                     </label>
-                </div>
-                <div>
-                    <label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Highlights/Balayage
                         <input
                             type="checkbox"
                             name="hb"
                             checked={formData.hb}
                             onChange={(e) => toggle(e.target.name)}
-                        ></input>
+                            className="custom-checkbox"
+                        />
                     </label>
-                    <label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Toner/Gloss
                         <input
                             type="checkbox"
                             name="tg"
                             checked={formData.tg}
                             onChange={(e) => toggle(e.target.name)}
-                        ></input>
+                            className="custom-checkbox"
+                        />
                     </label>
                 </div>
             </div>
