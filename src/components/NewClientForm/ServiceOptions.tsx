0e85d5748deafd3d3fa2bde6146a497426662cd5
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-07 08:26:53 -0700

    first section of NC form finished 🔁

--- src/components/NewClientForm/ServiceOptions.tsx
+++ src/components/NewClientForm/ServiceOptions.tsx
@@ -21,62 +21,62 @@ export default function ServiceOptions() {
     };
 
     return (
-        <form>
-            <div className="font-quattrocento text-3xl text-white ">
-                Which services do you see yourself using?
-                <label>(Select all that you might want)</label>
-                <div className="flex gap-10">
-                    <label className="flex cursor-pointer items-center justify-center gap-5">
-                        Haircuts
-                        <input
-                            type="checkbox"
-                            name="haircuts"
-                            checked={formData.haircuts}
-                            onChange={(e) => toggle(e.target.name)}
-                            className="custom-checkbox"
-                        />
-                    </label>
-                    <label className="flex cursor-pointer items-center justify-center gap-5">
-                        Color
-                        <input
-                            type="checkbox"
-                            name="color"
-                            checked={formData.color}
-                            onChange={(e) => toggle(e.target.name)}
-                            className="custom-checkbox"
-                        />
-                    </label>
-                    <label className="flex cursor-pointer items-center justify-center gap-5">
-                        Vivid
-                        <input
-                            type="checkbox"
-                            name="vivid"
-                            checked={formData.vivid}
-                            onChange={(e) => toggle(e.target.name)}
-                            className="custom-checkbox"
-                        />
-                    </label>
-                    <label className="flex cursor-pointer items-center justify-center gap-5">
-                        Highlights/Balayage
-                        <input
-                            type="checkbox"
-                            name="hb"
-                            checked={formData.hb}
-                            onChange={(e) => toggle(e.target.name)}
-                            className="custom-checkbox"
-                        />
-                    </label>
-                    <label className="flex cursor-pointer items-center justify-center gap-5">
-                        Toner/Gloss
-                        <input
-                            type="checkbox"
-                            name="tg"
-                            checked={formData.tg}
-                            onChange={(e) => toggle(e.target.name)}
-                            className="custom-checkbox"
-                        />
-                    </label>
-                </div>
+        <form className="flex flex-col items-center justify-center font-quattrocento text-3xl text-white">
+            <div className="flex gap-5">
+                <div>Which services do you see yourself using? </div>
+                <div className="text-2xl">(Select all that you might want)</div>
+            </div>
+            <div className="flex gap-10">
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Haircuts
+                    <input
+                        type="checkbox"
+                        name="haircuts"
+                        checked={formData.haircuts}
+                        onChange={(e) => toggle(e.target.name)}
+                        className="custom-checkbox"
+                    />
+                </label>
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Color
+                    <input
+                        type="checkbox"
+                        name="color"
+                        checked={formData.color}
+                        onChange={(e) => toggle(e.target.name)}
+                        className="custom-checkbox"
+                    />
+                </label>
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Vivid
+                    <input
+                        type="checkbox"
+                        name="vivid"
+                        checked={formData.vivid}
+                        onChange={(e) => toggle(e.target.name)}
+                        className="custom-checkbox"
+                    />
+                </label>
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Highlights/Balayage
+                    <input
+                        type="checkbox"
+                        name="hb"
+                        checked={formData.hb}
+                        onChange={(e) => toggle(e.target.name)}
+                        className="custom-checkbox"
+                    />
+                </label>
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Toner/Gloss
+                    <input
+                        type="checkbox"
+                        name="tg"
+                        checked={formData.tg}
+                        onChange={(e) => toggle(e.target.name)}
+                        className="custom-checkbox"
+                    />
+                </label>
             </div>
         </form>
     );
