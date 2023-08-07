373b240ebca3abe88f59f6f9252203f18b822034
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-07 09:17:06 -0700

    color history page styled 👕

--- src/components/NewClientForm/ColorHistory.tsx
+++ src/components/NewClientForm/ColorHistory.tsx
@@ -33,60 +33,66 @@ export default function ColorHistory() {
     };
 
     return (
-        <form>
-            <div>
+        <form className="flex flex-col items-center  font-quattrocento text-3xl text-white">
+            <div className="flex justify-center">
                 Have you had color before?
-                <div>
-                    <label>
+            </div>
+            <div>
+                <div className=" mb-5 mt-5 flex justify-center gap-10">
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         Yes
                         <input
                             type="checkbox"
                             name="yes"
                             checked={formData.yes}
                             onChange={() => toggle("yes")}
+                            className="custom-checkbox"
                         ></input>
                     </label>
-                    <label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
                         No
                         <input
                             type="checkbox"
                             name="no"
                             checked={formData.no}
                             onChange={() => toggle("no")}
+                            className="custom-checkbox"
                         ></input>
                     </label>
                 </div>
-                {formData.yes && (
-                    <div>
-                        <label>
-                            How long ago?
-                            <input
-                                name="ago"
-                                value={formData.ago}
-                                onChange={() => setAgo("ago")}
-                            ></input>
-                        </label>
-                        <label>
-                            Professionally
-                            <input
-                                type="checkbox"
-                                name="prof"
-                                checked={formData.prof}
-                                onChange={() => toggle("prof")}
-                            ></input>
-                        </label>
-                        <label>
-                            At home
-                            <input
-                                type="checkbox"
-                                name="home"
-                                checked={formData.home}
-                                onChange={() => toggle("home")}
-                            ></input>
-                        </label>
-                    </div>
-                )}
             </div>
+            {formData.yes && (
+                <div className="flex gap-5">
+                    <label>How long ago?</label>
+                    <input
+                        name="ago"
+                        type="input"
+                        value={formData.ago}
+                        onChange={(e) => setAgo(e.target.value)}
+                        className=" rounded-md p-2 text-xl text-purple-300 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-200"
+                    ></input>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        Professionally
+                        <input
+                            type="checkbox"
+                            name="prof"
+                            checked={formData.prof}
+                            onChange={() => toggle("prof")}
+                            className="custom-checkbox"
+                        ></input>
+                    </label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        At home
+                        <input
+                            type="checkbox"
+                            name="home"
+                            checked={formData.home}
+                            onChange={() => toggle("home")}
+                            className="custom-checkbox"
+                        ></input>
+                    </label>
+                </div>
+            )}
         </form>
     );
 }
