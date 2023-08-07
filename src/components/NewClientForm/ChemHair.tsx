4ff5416611de395476b18ffd1f60107dd2c43b84
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-07 09:21:23 -0700

    finished styling for chem hair 🥇

--- src/components/NewClientForm/ChemHair.tsx
+++ src/components/NewClientForm/ChemHair.tsx
@@ -25,61 +25,65 @@ export default function ChemHair() {
     };
 
     return (
-        <form>
-            <div>
-                Have you had any chemical treatments done on your hair?
-                <div>
-                    <label>
-                        Yes
+        <form className="flex flex-col items-center justify-center font-quattrocento text-3xl text-white">
+            <div>Have you had any chemical treatments done on your hair?</div>
+
+            <div className=" mb-5 mt-5 flex justify-center gap-10">
+                <label className="flex cursor-pointer items-center justify-center gap-5">
+                    Yes
+                    <input
+                        type="checkbox"
+                        name="yes"
+                        checked={formData.yes}
+                        onChange={() => toggle("yes")}
+                        className="custom-checkbox"
+                    ></input>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        No
                         <input
                             type="checkbox"
-                            name="yes"
-                            checked={formData.yes}
-                            onChange={() => toggle("yes")}
+                            name="no"
+                            checked={formData.no}
+                            onChange={() => toggle("no")}
+                            className="custom-checkbox"
                         ></input>
-                        <label>
-                            No
-                            <input
-                                type="checkbox"
-                                name="no"
-                                checked={formData.no}
-                                onChange={() => toggle("no")}
-                            ></input>
-                        </label>
+                    </label>
+                </label>
+            </div>
+            {formData.yes && (
+                <div className=" mb-5 mt-5 flex justify-center gap-10">
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        Keratin Treatment
+                        <input
+                            type="checkbox"
+                            name="keratin"
+                            checked={formData.keratin}
+                            onChange={() => toggle("keratin")}
+                            className="custom-checkbox"
+                        ></input>
+                    </label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        Permanent waves
+                        <input
+                            type="checkbox"
+                            name="waves"
+                            checked={formData.waves}
+                            onChange={() => toggle("waves")}
+                            className="custom-checkbox"
+                        ></input>
+                    </label>
+                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                        Relaxers
+                        <input
+                            type="checkbox"
+                            name="relaxers"
+                            checked={formData.relaxers}
+                            onChange={() => toggle("relaxers")}
+                            className="custom-checkbox"
+                        ></input>
                     </label>
                 </div>
-                {formData.yes && (
-                    <div>
-                        <label>
-                            Keratin Treatment
-                            <input
-                                type="checkbox"
-                                name="keratin"
-                                checked={formData.keratin}
-                                onChange={() => toggle("keratin")}
-                            ></input>
-                        </label>
-                        <label>
-                            Permanent waves
-                            <input
-                                type="checkbox"
-                                name="waves"
-                                checked={formData.waves}
-                                onChange={() => toggle("waves")}
-                            ></input>
-                        </label>
-                        <label>
-                            Relaxers
-                            <input
-                                type="checkbox"
-                                name="relaxers"
-                                checked={formData.relaxers}
-                                onChange={() => toggle("relaxers")}
-                            ></input>
-                        </label>
-                    </div>
-                )}
-            </div>
+            )}
         </form>
     );
 }
