00545cacf7746e1e7dffecec9d5350295f4673c9
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-08-13 11:38:07 -0700

    ➕ updated review route debugged ➕

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -92,20 +92,26 @@ export const reviewRouter = createTRPCRouter({
                 input;
 
             if (ctx.session.user.id === userId) {
+                const updatedReview = await ctx.prisma.review.update({
+                    where: {
+                        id: id,
+                    },
+                    data: { text, starRating },
+                });
+
                 if (images && images.length > 0) {
-                    const createdImages = await Promise.all(images.map(async (image) => {
-                        return ctx.prisma.images.create({
-                            data: {
-                                link: image.link,
-                                resourceType: "REVIEW",
-                                resourceId: id,
-                                userId: userId,
-                            },
-                        });
-                    }));
-                    return {
-                        createdImages,
-                    };
+                    await Promise.all(
+                        images.map(async (image) => {
+                            return ctx.prisma.images.create({
+                                data: {
+                                    link: image.link,
+                                    resourceType: "REVIEW",
+                                    resourceId: id,
+                                    userId: userId,
+                                },
+                            });
+                        })
+                    );
                 }
                 if (deleteImageIds && deleteImageIds.length > 0) {
                     const images = await ctx.prisma.images.findMany({
@@ -134,13 +140,6 @@ export const reviewRouter = createTRPCRouter({
                         },
                     });
                 }
-
-                const updatedReview = await ctx.prisma.review.update({
-                    where: {
-                        id: id,
-                    },
-                    data: { text, starRating },
-                });
 
                 return updatedReview;
             }
@@ -195,48 +194,3 @@ export const reviewRouter = createTRPCRouter({
             throw new Error("Invalid userId");
         }),
 });
-
-// delete: protectedProcedure
-// .input(
-//     z.object({
-//         imageIds: z.array(z.string()),
-//         userId: z.string(),
-//     })
-// )
-// .mutation(async ({ input, ctx }) => {
-//     const { imageIds, userId } = input;
-
-//     if (ctx.session.user.id === userId) {
-//         const images = await ctx.prisma.images.findMany({
-//             where: {
-//                 id: { in: imageIds },
-//             },
-//         });
-
-//         const removeFilePromises = images.map(async (image) => {
-//             try {
-//                 await removeFileFromS3(image.link);
-//             } catch (err) {
-//                 console.error(`Failed to remove file from S3: ${err}`);
-//                 throw new Error(
-//                     `Failed to remove file from S3: ${err}`
-//                 );
-//             }
-//         });
-
-//         await Promise.all(removeFilePromises);
-
-//         await ctx.prisma.images.deleteMany({
-//             where: {
-//                 id: { in: imageIds },
-//             },
-//         });
-
-//         return "Successfully deleted";
-//     } else {
-//         throw new Error(
-//             "You do not have permission to delete these images"
-//         );
-//     }
-// }),
-// });
