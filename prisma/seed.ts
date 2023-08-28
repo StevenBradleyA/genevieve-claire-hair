a1325c8cc66dac6f350e7418b1d00a5bac8d505e
Zaviar Brown <brown.zaviar@gmail.com>
2023-08-27 17:49:10 -0700

    Create seed data for services 💅

---
+++ prisma/seed.ts
@@ -0,0 +1,146 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    //? Blonding
+    await prisma.serviceCategory.create({
+        data: {
+            name: "Blonding",
+            subcategories: {
+                create: [
+                    {
+                        name: "Partial",
+                        price: 170,
+                        time: 180,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Full",
+                        price: 220,
+                        time: 210,
+                        bundleTime: 30,
+                    },
+                ],
+            },
+        },
+    });
+
+    //? All Over Color
+    await prisma.serviceCategory.create({
+        data: {
+            name: "All Over Color",
+            subcategories: {
+                create: [
+                    {
+                        name: "Gloss and toner only",
+                        price: 50,
+                        time: 60,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Roots only",
+                        price: 80,
+                        time: 90,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Roots to ends",
+                        price: 115,
+                        time: 120,
+                        bundleTime: 30,
+                    },
+                ],
+            },
+        },
+    });
+
+    //? Haircut
+    await prisma.serviceCategory.create({
+        data: {
+            name: "Haircut",
+            subcategories: {
+                create: [
+                    {
+                        name: "Buzz",
+                        price: 20,
+                        time: 20,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Short",
+                        price: 35,
+                        time: 35,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Long",
+                        price: 60,
+                        time: 60,
+                        bundleTime: 30,
+                    },
+                    {
+                        name: "Transformative",
+                        price: 90,
+                        time: 90,
+                        bundleTime: 60,
+                    },
+                ],
+            },
+        },
+    });
+
+    //? Styling
+    await prisma.serviceCategory.create({
+        data: {
+            name: "Styling",
+            subcategories: {
+                create: [
+                    {
+                        name: "Blowout",
+                        price: 45, // starting at
+                        time: 40,
+                        bundleTime: 60,
+                    },
+                    {
+                        name: "Special Event",
+                        price: 65,
+                        time: 90,
+                        bundleTime: 60,
+                    },
+                ],
+            },
+        },
+    });
+
+    // //? Geni
+    // await prisma.user.create({
+    //     data: {
+    //         firstName: "Geni",
+    //         lastName: "Evanson",
+    //         email: "genevieveclare.hair@outlook.com",
+    //         isAdmin: true,
+    //         isNew: false,
+    //     },
+    // });
+
+    // //? Zaviar
+    // await prisma.user.create({
+    //     data: {
+    //         firstName: "Zaviar",
+    //         lastName: "Brown",
+    //         email: "brown.zaviar@gmail.com",
+    //         isAdmin: true,
+    //         isNew: false,
+    //     },
+    // });
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(() => {
+        void prisma.$disconnect();
+    });
