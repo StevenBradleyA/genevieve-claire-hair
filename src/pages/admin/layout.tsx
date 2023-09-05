e7c4a3ee9bcce4c6ef5100ed9adcba45d7f84ae5
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-05 11:40:21 -0700

    🔧 Admin layout styling 🎨

--- src/pages/admin/layout.tsx
+++ src/pages/admin/layout.tsx
@@ -3,13 +3,23 @@ import type { ReactNode } from "react";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="flex">
-            <div className="flex flex-col">
+        // <div className="flex">
+        //     <div className="flex flex-col">
+        //         <h2 className="text-5xl">Sidebar</h2>
+        //         <Link href="/admin/users">Users</Link>
+        //         <Link href="/admin/services">Services</Link>
+        //     </div>
+        //     {children}
+        // </div>
+        <div className="grid grid-cols-10">
+            <div className="col-span-1"></div>
+            <div className="col-span-1 flex flex-col items-center">
                 <h2 className="text-5xl">Sidebar</h2>
                 <Link href="/admin/users">Users</Link>
                 <Link href="/admin/services">Services</Link>
             </div>
-            {children}
+            <div className="col-span-7 flex justify-center">{children}</div>
+            <div className="col-span-1"></div>
         </div>
     );
 }
