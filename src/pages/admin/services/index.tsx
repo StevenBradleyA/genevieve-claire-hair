b56ac43a8f2786a45947ac307b408257cc5117cc
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-05 11:45:30 -0700

    🔧 Layout for admin view of services

--- src/pages/admin/services/index.tsx
+++ src/pages/admin/services/index.tsx
@@ -1,3 +1,13 @@
-export default function Services() {
+import type { ReactElement } from "react";
+import type { NextPageWithLayout } from "~/pages/_app";
+import AdminLayout from "../layout";
+
+const AdminViewServices: NextPageWithLayout = () => {
     return <div>Hey</div>;
-}
+};
+
+AdminViewServices.getLayout = function getLayout(page: ReactElement) {
+    return <AdminLayout>{page}</AdminLayout>;
+};
+
+export default AdminViewServices;
