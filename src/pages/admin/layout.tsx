feaf78655b09006cede0aceb973fe978ca9b6202
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-10 14:34:56 -0700

    Add bookings to AdminLayout

--- src/pages/admin/layout.tsx
+++ src/pages/admin/layout.tsx
@@ -3,22 +3,17 @@ import type { ReactNode } from "react";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
-        // <div className="flex">
-        //     <div className="flex flex-col">
-        //         <h2 className="text-5xl">Sidebar</h2>
-        //         <Link href="/admin/users">Users</Link>
-        //         <Link href="/admin/services">Services</Link>
-        //     </div>
-        //     {children}
-        // </div>
-        <div className="grid grid-cols-10">
+        <div className="grid grid-cols-12">
             <div className="col-span-1"></div>
             <div className="col-span-1 flex flex-col items-center">
                 <h2 className="text-5xl">Sidebar</h2>
                 <Link href="/admin/users">Users</Link>
                 <Link href="/admin/services">Services</Link>
+                <Link href="/admin/bookings">Bookings</Link>
             </div>
-            <div className="col-span-7 flex justify-center">{children}</div>
+            <div className="col-span-9 flex flex-col items-center gap-5">
+                {children}
+            </div>
             <div className="col-span-1"></div>
         </div>
     );
