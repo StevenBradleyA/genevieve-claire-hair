ce7cb8f4055a13f53bad6336c1f53d61321f8670
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-10 14:33:55 -0700

    ClientBookings admin page

---
+++ src/components/Clients/ClientBookings.tsx
@@ -0,0 +1,25 @@
+import { DotLoader } from "react-spinners";
+import { api } from "~/utils/api";
+import BookingCard from "../Bookings/Display/BookingCard";
+
+export default function ClientBookings({ userId }: { userId: string }) {
+    const { data, isLoading } = api.booking.getByUserId.useQuery(userId);
+
+    if (isLoading)
+        return (
+            <div className=" mt-10 flex flex-col items-center justify-center gap-16">
+                <div className="text-lg text-white">Loading User</div>{" "}
+                <DotLoader size={50} color={"#ffffff"} loading={isLoading} />
+            </div>
+        );
+
+    if (!data) return <div>Oops</div>;
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-y-8 rounded-2xl bg-glass p-10 text-white shadow-2xl">
+            {data.map((booking) => (
+                <BookingCard key={booking.id} booking={booking} />
+            ))}
+        </div>
+    );
+}
