622b4f6b5c5d990388c7f0d45a3ea3b05a8cb8fd
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-10 14:36:18 -0700

    WIP: Admin view for Services

--- src/pages/admin/services/index.tsx
+++ src/pages/admin/services/index.tsx
@@ -1,9 +1,48 @@
+import AdminLayout from "../layout";
+import { api } from "~/utils/api";
 import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "~/pages/_app";
-import AdminLayout from "../layout";
+import type { ServiceSubcategory } from "@prisma/client";
 
 const AdminViewServices: NextPageWithLayout = () => {
-    return <div>Hey</div>;
+    const { data } = api.service.getAll.useQuery();
+
+
+    return (
+        <div>
+            {data &&
+                data.map((el) => {
+                    return (
+                        <div key={el.id}>
+                            <div>{el.name}</div>
+                            {el.subcategories.length && (
+                                <SubcategoryView subsArr={el.subcategories} />
+                            )}
+                        </div>
+                    );
+                })}
+        </div>
+    );
+};
+
+const SubcategoryView = ({ subsArr }: { subsArr: ServiceSubcategory[] }) => {
+    return (
+        <ul>
+            {subsArr.map((el) => {
+                return (
+                    <li key={el.id}>
+                        {el.name}
+                        <div>${el.price}</div>
+                        <div>{el.time} minutes</div>
+                        <div>{el.bundleTime} minutes</div>
+                        <div>
+                            Require consult: {el.requireConsult ? "yes" : "no"}
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 };
 
 AdminViewServices.getLayout = function getLayout(page: ReactElement) {
