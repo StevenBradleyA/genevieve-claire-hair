f4a424ac2c9b923161350080ef0cc48b6213d22d
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-12 11:29:53 -0700

    Booking card handles delete

--- src/components/Bookings/Display/BookingCard.tsx
+++ src/components/Bookings/Display/BookingCard.tsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 import { format } from "date-fns";
 import type { Booking } from "@prisma/client";
 import UpdateBooking from "../Update";
-import DeleteBooking from "../Delete";
 import ModalDialog from "~/components/Modal";
+import { api } from "~/utils/api";
 
 export default function BookingCard({ booking }: { booking: Booking }) {
     const { data: session } = useSession();
     const [showDelete, setShowDelete] = useState(false);
-    const [showUpdate, setShowUpdate] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
     const openModal = () => {
@@ -22,6 +21,27 @@ export default function BookingCard({ booking }: { booking: Booking }) {
 
     const { startDate, endDate, type, status } = booking;
 
+    const ctx = api.useContext();
+
+    const { mutate } = api.booking.delete.useMutation({
+        onSuccess: () => {
+            void ctx.booking.getByUserId.invalidate();
+        },
+    });
+
+    const deleteBooking = () => {
+        setShowDelete(false);
+        if (session && session.user) {
+            const data = {
+                id: booking.id,
+                userId: session.user.id,
+            };
+            return mutate(data);
+        } else {
+            throw new Error("Hot Toast Incoming!!!");
+        }
+    };
+
     // TODO: Refactor update & delete to work as modals
 
     return (
@@ -32,35 +52,44 @@ export default function BookingCard({ booking }: { booking: Booking }) {
             </div>
             <div>{type}</div>
             <div>{status}</div>
-            <div className="mt-5 flex gap-5 text-5xl font-bold">
-                <button
-                    onClick={openModal}
-                    className="rounded-full bg-glass p-2 px-3 text-3xl shadow-sm"
-                >
-                    🧐
-                </button>
+            <div className="mt-5 flex justify-between gap-5 text-5xl font-bold">
+                {!showDelete && (
+                    <>
+                        <button
+                            onClick={openModal}
+                            className="rounded-full bg-glass p-2 px-3 text-3xl shadow-sm"
+                        >
+                            📝
+                        </button>
+                        <button
+                            onClick={() => setShowDelete(true)}
+                            className="rounded-full bg-glass p-2 px-3 text-3xl shadow-sm"
+                        >
+                            🗑️
+                        </button>
+                    </>
+                )}
+
+                {showDelete && (
+                    <>
+                        <button
+                            className="rounded-full bg-glass p-2 px-3 text-3xl shadow-sm"
+                            onClick={deleteBooking}
+                        >
+                            🔥
+                        </button>
+                        <button
+                            className="rounded-full bg-glass p-2 px-3 text-3xl shadow-sm"
+                            onClick={() => setShowDelete(false)}
+                        >
+                            ❎
+                        </button>
+                    </>
+                )}
             </div>
             {session && (
                 <ModalDialog isOpen={isModalOpen} onClose={closeModal}>
-                    <>
-                        {!showDelete && (
-                            <UpdateBooking
-                                booking={booking}
-                                session={session}
-                                showUpdate={showUpdate}
-                                setShowUpdate={setShowUpdate}
-                            />
-                        )}
-
-                        {!showUpdate && (
-                            <DeleteBooking
-                                id={booking.id}
-                                session={session}
-                                showDelete={showDelete}
-                                setShowDelete={setShowDelete}
-                            />
-                        )}
-                    </>
+                    <UpdateBooking booking={booking} session={session} />
                 </ModalDialog>
             )}
         </div>
