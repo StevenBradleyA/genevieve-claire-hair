5654cca99f522698bd6e5a5af3a3fde18f64da74
Zaviar Brown <brown.zaviar@gmail.com>
2023-09-13 12:01:26 -0700

    UpdateBooking rendering correctly

---
+++ src/components/Bookings/Update/ServiceSelector.tsx
@@ -0,0 +1,37 @@
+import type { NormalizedServicesType } from "~/server/api/routers/service";
+
+export default function ServiceSelector({
+    serviceData,
+}: {
+    serviceData: NormalizedServicesType;
+}) {
+    const mainServices = Object.keys(serviceData);
+
+    console.log(mainServices);
+
+    return (
+        <div className="flex gap-5 text-2xl">
+            {mainServices.map((main, i) => {
+                return (
+                    <div key={`${main}-${i}`}>
+                        <input type="checkbox" />
+                        <label>{main}</label>
+                        <div className="flex flex-col text-lg">
+                            {serviceData[main]?.subcategories &&
+                                serviceData[main]?.subcategories.map(
+                                    (sub, j) => {
+                                        return (
+                                            <div key={`${sub.name}-${j}`}>
+                                                <input type="checkbox" />
+                                                <label>{sub.name}</label>
+                                            </div>
+                                        );
+                                    }
+                                )}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
