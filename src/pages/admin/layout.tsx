cbe91d41a2a640e3d584d357f1475872a7d61185
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-09-18 09:30:45 -0700

    🆕 styled the nav bar and added new svgs 🆕

--- src/pages/admin/layout.tsx
+++ src/pages/admin/layout.tsx
@@ -1,17 +1,36 @@
 import Link from "next/link";
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
+import Image from "next/image";
+import left from "../../../public/svgs/chevron-left-solid.svg";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <div className="grid w-full grid-cols-12 gap-2">
-            <div className="col-span-3 flex flex-col items-center">
+        // <div className="grid w-full grid-cols-12 gap-2">
+        <div className=" flex w-full justify-between">
+            <div className=" flex w-full flex-col items-center">{children}</div>
+
+            <div
+                className="relative flex flex-col items-center rounded-3xl bg-glass p-10 shadow-xl"
+                onClick={toggleSidebar}
+            >
+                <div className="absolute left-5 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+                    <Image
+                        alt="menu-minimize"
+                        src={left}
+                        width={20}
+                        height={20}
+                    />
+                </div>
                 <h2 className="text-5xl">Sidebar</h2>
                 <Link href="/admin/users">Users</Link>
                 <Link href="/admin/services">Services</Link>
                 <Link href="/admin/bookings">Bookings</Link>
-            </div>
-            <div className="col-span-8 flex w-full flex-col items-center gap-5">
-                {children}
             </div>
         </div>
     );
