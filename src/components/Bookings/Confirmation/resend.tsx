e55988f35b7f635adb9612751e6980a12bbe81a5
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-09-29 16:39:51 -0700

    🏃 comments set up for email and text will start tomorrow! 🏃

--- src/components/Bookings/Confirmation/resend.tsx
+++ src/components/Bookings/Confirmation/resend.tsx
@@ -1,4 +1,8 @@
 export default function EmailConfirmation() {
+    // todo this needs to take in a date / email
+    // send an immediate confirmation email
+    // send a one week reminder email if the date is more than 7 days away
+    // send a one day reminder email
     return (
         <div>
             <div></div>
