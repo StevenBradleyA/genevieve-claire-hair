4eeda9037c5bf00218d2f8dfd7be9981a04ee70e
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-04 10:32:55 -0700

    🎁 created new modal for admin creating booking 🎁

---
+++ src/components/Bookings/AdminCreate/index.tsx
@@ -0,0 +1,15 @@
+interface AdminCreateBookingProps {
+    closeModal: () => void;
+    userId: string;
+}
+
+export default function AdminCreateBooking({
+    closeModal,
+    userId,
+}: AdminCreateBookingProps) {
+    return (
+        <>
+            <div>hello there</div>;
+        </>
+    );
+}
