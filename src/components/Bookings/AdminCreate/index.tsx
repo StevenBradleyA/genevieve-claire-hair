1bd4194db557ef24862a4d890b2877c16c82d734
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-04 13:07:24 -0700

    🔬 finished the type selection for admin booking 🔬

--- src/components/Bookings/AdminCreate/index.tsx
+++ src/components/Bookings/AdminCreate/index.tsx
@@ -17,21 +17,57 @@ export default function AdminCreateBooking({
     // todo error handling for selecting a service
 
     const [type, setType] = useState<string>("");
-    const [isBlondingSelected, setIsBlondingSelected] =
-        useState<boolean>(false);
-    const [isVividSelected, setIsVividSelected] = useState<boolean>(false);
-    const [isHaircutSelected, setIsHaircutSelected] = useState<boolean>(false);
-    const [isColorCorrectionSelected, setIsColorCorrectionSelected] =
-        useState<boolean>(false);
-    const [selectedService, setSelectedService] = useState<ServicesType | null>(
-        null
-    );
+
+    const [selectedServices, setSelectedServices] = useState<string[]>([]);
+
     const { data: serviceData } = api.service.getAllNormalized.useQuery();
 
-    console.log(serviceData);
+    console.log(selectedServices);
 
-    const handleServiceSelect = (service: ServicesType) => {
-        setSelectedService(service);
+    const handleServiceSelect = (
+        service: ServicesType,
+        subcategory?: string
+    ) => {
+        const serviceName = subcategory
+            ? `${service.name}: ${subcategory}`
+            : service.name;
+
+        if (selectedServices.includes(serviceName)) {
+            // If service with this subcategory is already selected, unselect it
+            setSelectedServices((prevServices) =>
+                prevServices.filter(
+                    (prevService) => prevService !== serviceName
+                )
+            );
+        } else {
+            const serviceType = service.name;
+            const servicesWithSameType = selectedServices.filter((service) =>
+                service.startsWith(serviceType)
+            );
+
+            if (servicesWithSameType.length > 0) {
+                setSelectedServices((prevServices) =>
+                    prevServices.filter(
+                        (prevService) => !prevService.startsWith(serviceType)
+                    )
+                );
+            }
+
+            // Add the selected service with subcategory
+            setSelectedServices((prevServices) => [
+                ...prevServices,
+                serviceName,
+            ]);
+        }
+    };
+
+    const handleClearSelection = () => {
+        // Clears all selected services
+        setSelectedServices([]);
+        const selects = document.querySelectorAll("select");
+        selects.forEach((select) => {
+            select.value = "";
+        });
     };
 
     return (
@@ -39,28 +75,57 @@ export default function AdminCreateBooking({
             <div className="flex gap-2">
                 {serviceData &&
                     Object.values(serviceData).map((service: ServicesType) => (
-                        <button
-                            key={service.id}
-                            onClick={() => handleServiceSelect(service)}
-                            className="rounded-2xl bg-darkGlass px-6 py-2 text-xl"
-                        >
-                            {service.name}
-                        </button>
+                        <div key={service.id}>
+                            {service.subcategories.length > 0 ? (
+                                <select
+                                    className="rounded-2xl bg-darkGlass px-6 py-2 text-xl"
+                                    onChange={(e) =>
+                                        handleServiceSelect(
+                                            service,
+                                            e.target.value
+                                        )
+                                    }
+                                >
+                                    <option value="" disabled selected>
+                                        {service.name}
+                                    </option>
+                                    {service.subcategories.map(
+                                        (subcategory) => (
+                                            <option
+                                                key={subcategory.id}
+                                                value={subcategory.name}
+                                            >
+                                                {subcategory.name}
+                                            </option>
+                                        )
+                                    )}
+                                </select>
+                            ) : (
+                                <button
+                                    key={service.id}
+                                    onClick={() => handleServiceSelect(service)}
+                                    className="rounded-2xl px-6 py-2 text-xl"
+                                    style={{
+                                        backgroundColor:
+                                            selectedServices.includes(
+                                                service.name
+                                            )
+                                                ? "blue" // Change to your desired background color for selected services
+                                                : "gray", // Change to your desired background color for unselected services
+                                    }}
+                                >
+                                    {service.name}
+                                </button>
+                            )}
+                        </div>
                     ))}
             </div>
-
-            {selectedService && selectedService.subcategories.length > 0 && (
-                <div>
-                    <h3>Subcategories for {selectedService.name}:</h3>
-                    <ul>
-                        {selectedService.subcategories.map(
-                            (subcategory: ServicesType.subcategories) => (
-                                <li key={subcategory.id}>{subcategory.name}</li>
-                            )
-                        )}
-                    </ul>
-                </div>
-            )}
+            <button
+                onClick={handleClearSelection}
+                className="mt-4 rounded-2xl px-6 py-2 text-xl"
+            >
+                Clear Selection
+            </button>
         </div>
     );
 }
