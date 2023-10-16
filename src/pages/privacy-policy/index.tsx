9105057344a9c19ceaea14ce5f6abde0d83147d7
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 14:46:14 -0700

    added review and services to TOS 🔍

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -23,6 +23,21 @@ export default function PrivacyPolicy() {
                     <div>Effective 10-15-2023</div>
                     <div>Last updated on 10-15-2023</div>
                 </div>
+
+                <div className="mb-5 text-4xl ">
+                    Personal Information Request
+                </div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
+                    You have the right to request access to the personal
+                    information we collect from you, change that information, or
+                    delete it. To request to review, update, or delete your
+                    personal information, please visit: email
+                    genevieveclare.hair@outlook.com to request data stored.
+                </p>
+
+                <div className="mb-5 text-4xl "> Cookie Policy</div>
+                <div>this site uses cookies to check if you are logged in </div>
+
             </div>
             <Footer />
         </>
