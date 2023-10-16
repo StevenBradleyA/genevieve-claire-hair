6d98ca0bf39bdb861640a637f68127766e19243c
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 14:10:45 -0700

    🔬 added new Review policy to terms of service 🔬

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -7,6 +7,18 @@ export default function PrivacyPolicy() {
                 <div className="flex justify-center text-5xl ">
                     Privacy Policy
                 </div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                        By accessing this Website, accessible from
+                        genevieveclare.hair, you are agreeing to be bound by
+                        these Website Terms of Service and agree that you are
+                        responsible for the agreement in accordance with any
+                        applicable local laws. IF YOU DO NOT AGREE TO ALL THE
+                        TERMS AND CONDITIONS OF THIS AGREEMENT, YOU ARE NOT
+                        PERMITTED TO ACCESS OR USE OUR SERVICES.
+                    </p>
+                </p>
+
                 <div className="mb-5 flex justify-between text-white">
                     <div>Effective 10-15-2023</div>
                     <div>Last updated on 10-15-2023</div>
