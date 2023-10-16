c4bbe8abbbd203589fdd955e970a2aa853d15c60
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 12:14:06 -0700

    🎈 added footer to TOS and PrivacyPolicy 🎈

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,13 +1,18 @@
+import Footer from "~/components/HomePage/footer";
+
 export default function PrivacyPolicy() {
     return (
-        <div className="w-1/2 text-violet-300">
-            <div className="flex justify-center text-5xl ">
-                Terms of Service
+        <>
+            <div className="w-1/2 text-violet-300">
+                <div className="flex justify-center text-5xl ">
+                    Privacy Policy
+                </div>
+                <div className="mb-5 flex justify-between text-white">
+                    <div>Effective 10-15-2023</div>
+                    <div>Last updated on 10-15-2023</div>
+                </div>
             </div>
-            <div className="mb-5 flex justify-between text-white">
-                <div>Effective 10-15-2023</div>
-                <div>Last updated on 10-15-2023</div>
-            </div>
-        </div>
+            <Footer />
+        </>
     );
 }
