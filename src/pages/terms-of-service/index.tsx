8f344ff9cb5a7eadefdccebbfab567946686197e
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 11:02:17 -0700

    🥇 created terms of service and privacy policy pages 🥇

---
+++ src/pages/terms-of-service/index.tsx
@@ -0,0 +1,8 @@
+export default function TermsOfService() {
+    return (
+        <div>
+            <div> hello there old sport </div>
+            <div>this site uses cookies to check if you are logged in </div>
+        </div>
+    );
+}
