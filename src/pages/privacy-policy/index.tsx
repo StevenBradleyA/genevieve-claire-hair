5e0d0d262210a3d4ac9c726b4d6b9beb3fba2ab1
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 15:50:48 -0700

    🥞 added cookie policy 🥞

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -102,25 +102,20 @@ export default function PrivacyPolicy() {
                 <div className="mb-5 text-4xl ">
                     Your Choices Regarding Cookies
                 </div>
-                <div className="mb-2 text-xl ">Improving Our Services</div>
+                <div className="mb-2 text-xl ">Disabling Cookies</div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
-                    {`We analyze usage data to improve the functionality and user experience of the Website and Services.`}
+                    {`You can choose to disable cookies through your individual browser options. However, please note that if you disable cookies, you may not be able to use some portions of our Website properly.`}
                 </p>
-                <div className="mb-2 text-xl ">Improving Our Services</div>
+                <div className="mb-2 text-xl ">Opting Out</div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
-                    {`We analyze usage data to improve the functionality and user experience of the Website and Services.`}
-                </p>
-
-                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
-                    {`Genevieve Clare Hair stores your data securly using Prisma`}
+                    {`You can opt-out of cookies by adjusting the settings in your web browser. Please refer to your browser's "help" section or a similar feature to learn more about how to manage cookie preferences.`}
                 </p>
 
                 <div className="mb-5 text-4xl ">
                     What Are Your Data Protection Rights?
                 </div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
-                    {` Genevieve Clare Hair would like to make sure you are fully aware of
-                    all of your data protection rights. Every user is entitled
+                    {` Every user is entitled
                     to the following: The right to access. You have the right
                     to request Genevieve Clare Hair for copies of your personal data. We
                     may limit the number of times this request can be made to
@@ -152,9 +147,16 @@ export default function PrivacyPolicy() {
                     personal information, please visit: email
                     genevieveclare.hair@outlook.com to request data stored.
                 </p>
-
-                <div className="mb-5 text-4xl "> Cookie Policy</div>
-                <div>this site uses cookies to check if you are logged in </div>
+                <div className="mb-5 text-4xl ">
+                    Privacy Policy Modification
+                </div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
+                    {`Genevieveclare.hair may revise this Privacy Policy for its
+                    Website at any time without prior notice. The updated
+                    version will be indicated by an updated "Last Updated" date.
+                    We encourage you to review this Cookie Policy periodically
+                    to stay informed about how we use cookies.`}
+                </p>
             </div>
             <Footer />
         </>
