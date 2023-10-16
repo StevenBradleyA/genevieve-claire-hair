329309393871faadc7bd82c57ed65d3a6abc0969
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 11:34:26 -0700

    added multiple policies to TOS ✈️

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,7 +1,13 @@
 export default function PrivacyPolicy() {
     return (
-        <div>
-            <div> privacy policy </div>
+        <div className="w-1/2 text-violet-300">
+            <div className="flex justify-center text-5xl ">
+                Terms of Service
+            </div>
+            <div className="mb-5 flex justify-between text-white">
+                <div>Effective 10-15-2023</div>
+                <div>Last updated on 10-15-2023</div>
+            </div>
         </div>
     );
 }
