f44340b2ac6afd753cf59390122726a122485e4e
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-16 14:10:45 -0700

    🔬 added new Review policy to terms of service 🔬

--- src/pages/terms-of-service/index.tsx
+++ src/pages/terms-of-service/index.tsx
@@ -1,6 +1,11 @@
 import Footer from "~/components/HomePage/footer";
 
 export default function TermsOfService() {
+    // todo geni has the ability to delete any review
+    // todo geni has the right to refuse service. She can cancel any booking
+    // todo comment policy () - if you'd like to appeal
+    // todo cancellation policy
+
     return (
         <>
             <div className="w-1/2 text-violet-300">
@@ -12,32 +17,83 @@ export default function TermsOfService() {
                         Last updated on 10-15-2023
                     </div>
                 </div>
-
                 <div className="mb-5 text-4xl "> Agreement</div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
-                    By accessing this Website, accessible from
-                    genevieveclare.hair, you are agreeing to be bound by these
-                    Website Terms of Service and agree that you are responsible
-                    for the agreement in accordance with any applicable local
-                    laws. IF YOU DO NOT AGREE TO ALL THE TERMS AND CONDITIONS OF
-                    THIS AGREEMENT, YOU ARE NOT PERMITTED TO ACCESS OR USE OUR
-                    SERVICES.
+                    {` Welcome to Genevieve Clare Hair! These Terms of Service
+                    govern your use of our website located at genevieveclare.hair. 
+                    Please read these Terms of Service carefully
+                    before accessing or using the Website. By accessing this
+                    Website, accessible from genevieveclare.hair, you are
+                    agreeing to be bound by these Website Terms of Service and
+                    agree that you are responsible for the agreement in
+                    accordance with any applicable local laws. IF YOU DO NOT
+                    AGREE TO ALL THE TERMS AND CONDITIONS OF THIS AGREEMENT, YOU
+                    ARE NOT PERMITTED TO ACCESS OR USE OUR SERVICES.`}
                 </p>
-
+                <div className="mb-5 text-4xl "> Use of the Website</div>
+                <div className="mb-2 text-xl "> Eligibility</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {` You must be at least 18 years old to use the Website. By using the Website, you represent and warrant that you are at least 18 years old.`}
+                </p>
+                <div className="mb-2 text-xl "> Personal Information</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {` You agree to provide accurate, current, and complete information during the registration or appointment booking process and to update such information to keep it accurate, current, and complete.`}
+                </p>
+                <div className="mb-2 text-xl "> Prohibited Activities</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {` You may not use the Website for any illegal or unauthorized purpose. You may not, in the use of the Website, violate any applicable laws, regulations, or rights of others.`}
+                </p>
                 <div className="mb-5 text-4xl "> Links</div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
                     {`Genevieveclaire.hair is not responsible for the contents of any
                 linked sites. The use of any linked website is at the user's own
                 risk.`}
                 </p>
-                <div className="mb-5 text-4xl "> Changes</div>
+                <div className="mb-5 text-4xl ">
+                    {" "}
+                    Terms of Service Modification
+                </div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
                     Genevieveclare.hair may revise these Terms of Service for
                     its Website at any time without prior notice. By using this
                     Website, you are agreeing to be bound by the current version
                     of these Terms of Service.
                 </p>
-                <div className="mb-5 text-4xl "> Contact</div>
+                <div className="mb-5 text-4xl ">
+                    Personal Information Request
+                </div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
+                    You have the right to request access to the personal
+                    information we collect from you, change that information, or
+                    delete it. To request to review, update, or delete your
+                    personal information, please visit: email
+                    genevieveclarehair@outlook.com to request data stored.
+                </p>
+                <div className="mb-5 text-4xl "> Review Policy</div>
+                <div className="mb-2 text-xl "> Content Moderation</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {`At Genevieve Clare Hair, we value honest feedback and reviews from our customers. We encourage you to share your experiences on our Website. However, we reserve the right to moderate and remove any review that is deemed offensive, inappropriate, or violates our content guidelines.`}
+                </p>
+                <div className="mb-2 text-xl ">Offensive Content</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {`Reviews that contain offensive language, discriminatory remarks, or violate the privacy of our staff or other customers will be promptly removed. We believe in maintaining a respectful and positive online environment for everyone.`}
+                </p>{" "}
+                <div className="mb-2 text-xl ">Moderation Process </div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {`Our moderation process aims to ensure that the reviews displayed on our Website are constructive, helpful, and relevant to the experiences of our customers. While we encourage your honest opinions, we request that you express them in a respectful manner.`}
+                </p>{" "}
+                <div className="mb-2 text-xl "> Right to Remove</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {`We have the ability to remove any review that we find offensive to others. If you believe your review was removed in error, please contact our customer support team at genevieveclarehair@outlook.com to address your concerns.`}
+                </p>{" "}
+                <div className="mb-2 text-xl "> Feedback Welcome</div>
+                <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white ">
+                    {`We appreciate your understanding and cooperation in maintaining a respectful review environment. If you have any feedback or questions about our review policy, please feel free to reach out to us.`}
+                </p>{" "}
+                <div className="mb-5 text-4xl "> Disclaimer</div>
+                <div className="mb-5 text-4xl "> Privacy Policy</div>
+                <div className="mb-5 text-4xl "> Cookie Policy</div>
+                <div className="mb-5 text-4xl "> Cancellation Policy</div>
                 <div className="mb-5 text-4xl "> Limitations</div>
                 <p className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
                     To the fullest extent permitted by applicable law, [Your
@@ -49,10 +105,16 @@ export default function TermsOfService() {
                     to use the Website; (b) any unauthorized access to or use of
                     our servers and/or any personal information stored therein.
                 </p>
-                <div className="mb-5 text-4xl "> Disclaimer</div>
-                <div className="mb-5 text-4xl "> Privacy Policy</div>
-
                 <div>this site uses cookies to check if you are logged in </div>
+                <div className="mb-5 text-4xl "> Contact</div>
+                <div className="mb-5 rounded-2xl bg-darkGlass p-4 text-white">
+                    <h3>Genevieve Clare Hair</h3>
+                    <h3>12121 Northup Way</h3>
+                    <h3>Unit 214</h3>
+                    <h3>Bellevue, WA 98005</h3>
+                    <h3>(425) 241-7865</h3>
+                    <h3>genevieveclarehair@outlook.com</h3>
+                </div>
             </div>
             <Footer />
         </>
