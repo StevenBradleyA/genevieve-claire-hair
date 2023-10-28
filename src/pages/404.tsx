30f6abfc51a1dcf1bb0c70156a5f993480866c0b
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-27 17:24:01 -0700

    👕 finished styling the custom 404 page 👕

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,34 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion";
-
-
+import gif from "@public/Gifs/gch-404.gif";
 
 export default function Custom404() {
-
-
-
-
     return (
-    
-    <div className="w-2/3 text-white">
-
-
-
-    
-    
-    
-    <h1>404 - Page Not poggers</h1>
-    
-    
-    <motion.button
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.95 }}
-    className=" rounded-3xl bg-blue-200 px-6 py-2 text-3xl shadow-md "
->
-    <Link href="/">Home</Link>
-</motion.button>
-    
-    </div>
-    
+        <div className="flex w-2/3 flex-col items-center justify-center text-6xl text-white">
+            <Image src={gif} alt="404" className="w-3/4" />
+            <div className="flex flex-col items-center rounded-2xl bg-glass p-5 shadow-xl">
+                <div className="mb-2 text-6xl">
+                    Uh oh! Looks like you got lost
+                </div>
+                <div className="mb-5 text-4xl">
+                    No worries, the homepage is just a click away.
+                </div>
+                <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className=" rounded-3xl bg-blue-200 px-6 py-2 text-3xl shadow-md "
+                >
+                    <Link href="/">click me!</Link>
+                </motion.button>
+            </div>
+        </div>
     );
 }
