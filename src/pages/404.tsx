24e31a5ea92b5641b841e4790441189499104f2f
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-06 14:28:44 -0800

    👕 updated scrollbar styling for chrome 👕

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,13 +5,13 @@ import gif from "@public/Gifs/gch-404.gif";
 
 export default function Custom404() {
     return (
-        <div className="flex w-2/3 flex-col items-center justify-center text-6xl text-white">
+        <div className="flex w-2/3 flex-col items-center justify-center text-4xl text-white">
             <Image src={gif} alt="404" className="w-3/4" />
-            <div className="flex flex-col items-center rounded-2xl bg-glass p-5 shadow-xl">
-                <div className="mb-2 text-6xl">
+            <div className="mt-5 flex flex-col items-center rounded-2xl bg-glass p-5 shadow-xl">
+                <div className="mb-2 ">
                     Uh oh! Looks like you got lost
                 </div>
-                <div className="mb-5 text-4xl">
+                <div className="mb-5 ">
                     No worries, the homepage is just a click away.
                 </div>
                 <motion.button
