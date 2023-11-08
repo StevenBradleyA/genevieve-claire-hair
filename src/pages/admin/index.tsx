9420e6ee370238ca1a51f52917132268a322aac4
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-08 12:45:44 -0800

    💥 created a new svg component  💥

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,9 +2,11 @@ import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "../_app";
 import AdminLayout from "./layout";
 import { useSession } from "next-auth/react";
+import { displaySvg } from "~/components/Svgs";
 import Custom404 from "../404";
 import Image from "next/image";
 import logo from "@public/icons/heart.png";
+import { motion } from "framer-motion";
 
 const AdminPage: NextPageWithLayout = () => {
     // TODO add admin only viewing or redirect if user is not admin
@@ -23,12 +25,24 @@ const AdminPage: NextPageWithLayout = () => {
                 <div className="font-grand-hotel text-8xl">Admin </div>
                 <Image src={logo} alt="logo" width={100} height={100} />
             </div>
-            <div className="w-2/3 rounded-2xl bg-glass p-10 shadow-lg">
-                <div className="text-center"> Geni notes</div>
-                <div className="rounded-2xl bg-darkGlass p-5">
-                    {session?.user?.notes}
+
+            {session && session.user && (
+                <div className="w-2/3 rounded-2xl bg-glass p-10 shadow-lg">
+                    <div className="relative mb-1 flex w-full justify-center">
+                        <div className=""> {session.user.firstName} notes</div>
+                        <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="absolute bottom-1 right-0 rounded-xl bg-glass shadow-md"
+                        >
+                            {displaySvg("adminEdit")}
+                        </motion.button>
+                    </div>
+                    <div className="rounded-2xl bg-darkGlass p-5">
+                        {session.user.notes}
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
