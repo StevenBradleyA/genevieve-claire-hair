1b672f3229ade379766b7e2b198d86cbd9b99d48
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-09 09:31:40 -0800

    added text and email confirmation to admin ✅

--- src/components/Bookings/AdminCreate/index.tsx
+++ src/components/Bookings/AdminCreate/index.tsx
@@ -9,24 +9,20 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 
 interface AdminCreateBookingProps {
-    closeModal: () => void;
     userId: string;
     firstName: string;
     lastName: string;
 }
 
 export default function AdminCreateBooking({
-    // closeModal,
     userId,
     firstName,
     lastName,
 }: AdminCreateBookingProps) {
     // TODO CUSTOM TIME SELECTION -- KEEPS TRACK OF OTHER BOOKINGS BUT DOESNT HAVE SCHEDULE TIME CONSTRAINTS
     // TODO have default service times as well as custom????
-
     // todo show calendar and normal booking stuffs
     // todo error handling for selecting a service
-    // todo may want to pass user firstname and lastname so she knows who she is booking for
 
     const router = useRouter();
     const [selectedServices, setSelectedServices] = useState<string[]>([]);
@@ -36,6 +32,8 @@ export default function AdminCreateBooking({
     const [originalTime, setOriginalTime] = useState(0);
     const [customPrice, setCustomPrice] = useState(0);
     const [customTime, setCustomTime] = useState(0);
+    const [textSelect, setTextSelect] = useState<boolean>(false);
+    const [emailSelect, setEmailSelect] = useState<boolean>(true);
 
     const ctx = api.useContext();
     const { data: futureBookings } = api.booking.getFuture.useQuery();
@@ -54,6 +52,33 @@ export default function AdminCreateBooking({
                 },
             });
             void router.push("/bookings/confirmed");
+        },
+    });
+
+    const { mutate: sendEmail } = api.booking.sendEmailConfirmation.useMutation(
+        {
+            onSuccess: () => {
+                toast.success("Email Sent!", {
+                    icon: "👏",
+                    style: {
+                        borderRadius: "10px",
+                        background: "#333",
+                        color: "#fff",
+                    },
+                });
+            },
+        }
+    );
+    const { mutate: sendText } = api.booking.sendTextConfirmation.useMutation({
+        onSuccess: () => {
+            toast.success("Text Sent!", {
+                icon: "👏",
+                style: {
+                    borderRadius: "10px",
+                    background: "#333",
+                    color: "#fff",
+                },
+            });
         },
     });
 
@@ -80,6 +105,39 @@ export default function AdminCreateBooking({
 
             setDate(undefined);
             mutate(data);
+
+            const formattedDate = startDate.toLocaleString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+            });
+
+            if (emailSelect) {
+                const emailData = {
+                    userEmail: user.email as string,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    startDate,
+                    displayDate: formattedDate,
+                    type,
+                };
+                sendEmail(emailData);
+            }
+
+            if (textSelect) {
+                const textData = {
+                    phoneNumber: `+1${user.phoneNumber}`,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    displayDate: formattedDate,
+                    startDate,
+                    type,
+                };
+                sendText(textData);
+            }
         } else {
             throw new Error("Hot Toast Incoming!!!");
         }
@@ -130,7 +188,7 @@ export default function AdminCreateBooking({
         );
 
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center text-xl">
             <div className="mb-3 text-5xl">{`${firstName} ${lastName}`} </div>
             <AdminBookingSelectService
                 selectedServices={selectedServices}
@@ -154,7 +212,26 @@ export default function AdminCreateBooking({
                 totalTime={customTime}
                 bookedDates={futureBookings}
             />
-
+            {/* {session?.user.phoneNumber !== null && (
+                    <div className="my-5 flex gap-5 text-sm">
+                        <button
+                            className={`rounded-lg ${
+                                textSelect ? "bg-violet-300" : "bg-darkGlass"
+                            } px-4 py-2 `}
+                            onClick={() => setTextSelect(!textSelect)}
+                        >
+                            Text Confirmation
+                        </button>
+                        <button
+                            className={`rounded-lg ${
+                                emailSelect ? "bg-violet-300" : "bg-darkGlass"
+                            } px-4 py-2 `}
+                            onClick={() => setEmailSelect(!emailSelect)}
+                        >
+                            Email Confirmation
+                        </button>
+                    </div>
+                )} */}
             <button
                 disabled={checkConflicts()}
                 className="mt-4 rounded-lg bg-violet-300 px-4 py-2 transition-all duration-200 enabled:hover:scale-105 enabled:hover:bg-violet-300 disabled:bg-violet-200 disabled:text-slate-200"
