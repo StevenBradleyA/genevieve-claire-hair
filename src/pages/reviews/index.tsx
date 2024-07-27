ccf4b02bda9fa300f86c4cb7f6a6b968f4c01633
StevenbradleyA <andersonstevenbradley@gmail.com>
2024-07-27 15:01:49 -0700

    📱 updated mobile styling for reviews 📱

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -20,9 +20,83 @@ export default function Reviews() {
             <div className="flex w-full flex-col items-center">
                 {isMobile ? (
                     <div className="flex flex-col items-center gap-5">
-                        <h1 className="font-grandHotel text-6xl text-white ">
-                            Reviews
+                        <h1 className="relative mt-5 flex w-full justify-center font-archivo text-5xl text-white">
+                            REVIEWS
+                            <div className=" absolute right-10 flex flex-col gap-2">
+                                <Image
+                                    alt="heart logo"
+                                    src={heart}
+                                    className="image-black-to-white h-4 w-4 object-contain ease-in hover:opacity-70"
+                                />
+                                <Image
+                                    alt="heart logo"
+                                    src={heart}
+                                    className="image-black-to-white h-4 w-4 object-contain ease-in hover:opacity-70"
+                                />
+                                <Image
+                                    alt="heart logo"
+                                    src={heart}
+                                    className="image-black-to-white h-4 w-4 object-contain ease-in hover:opacity-70"
+                                />
+                            </div>
                         </h1>
+
+                        <div className="relative mt-5 flex h-12 w-full items-center justify-center bg-white">
+                            <div className="overflow-hidden">
+                                <motion.div
+                                    className="z-40 flex items-center gap-3 font-archivo text-xl  text-black"
+                                    animate={{ x: [0, -400] }}
+                                    transition={{
+                                        duration: 16,
+                                        ease: "linear",
+                                        repeat: Infinity,
+                                        repeatType: "loop",
+                                    }}
+                                >
+                                    <p className=" flex-shrink-0 bg-gradient-to-r from-indigo-200 via-fuchsia-200 to-purple-300 bg-clip-text text-transparent">
+                                        tell me what you think
+                                    </p>
+                                    <Image
+                                        alt="heart logo"
+                                        src={heart}
+                                        className="image-black-to-purple h-6 w-6 object-contain"
+                                    />
+                                    <p className=" flex-shrink-0 bg-gradient-to-r from-indigo-200 via-fuchsia-200 to-purple-300 bg-clip-text text-transparent">
+                                        tell me what you think
+                                    </p>
+                                    <Image
+                                        alt="heart logo"
+                                        src={heart}
+                                        className="image-black-to-purple h-6 w-6 object-contain"
+                                    />
+                                    <p className=" flex-shrink-0 bg-gradient-to-r from-indigo-200 via-fuchsia-200 to-purple-300 bg-clip-text text-transparent">
+                                        tell me what you think
+                                    </p>
+                                    <Image
+                                        alt="heart logo"
+                                        src={heart}
+                                        className="image-black-to-purple h-6 w-6 object-contain"
+                                    />
+                                    <p className=" flex-shrink-0 bg-gradient-to-r from-indigo-200 via-fuchsia-200 to-purple-300 bg-clip-text text-transparent">
+                                        tell me what you think
+                                    </p>
+                                    <Image
+                                        alt="heart logo"
+                                        src={heart}
+                                        className="image-black-to-purple h-6 w-6 object-contain"
+                                    />
+                                    <p className=" flex-shrink-0 bg-gradient-to-r from-indigo-200 via-fuchsia-200 to-purple-300 bg-clip-text text-transparent">
+                                        tell me what you think
+                                    </p>
+                                    <Image
+                                        alt="heart logo"
+                                        src={heart}
+                                        className="image-black-to-purple h-6 w-6 object-contain"
+                                    />
+                                </motion.div>
+                            </div>
+                        </div>
+
                         <div className="flex w-[400px] justify-center">
                             {session && session.user ? (
                                 <ChooseReview session={session} />
@@ -64,7 +138,7 @@ export default function Reviews() {
                                 className=" absolute -top-[30px] right-0  w-40"
                                 alt="left indent"
                             />
-                            <div className="absolute bottom-28 right-40 flex w-[400px] justify-center font-raleway">
+                            <div className="absolute bottom-16 right-40 flex w-[400px] justify-center font-raleway largeLaptop:bottom-28">
                                 {session && session.user ? (
                                     <ChooseReview session={session} />
                                 ) : (
